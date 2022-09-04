55809179e46e640f6fed3540e35166c1bf2ff829
goutam-git <goutamghosh@gmail.com>
2022-09-04 23:41:01 +0530

    04-09-2022

---
+++ src/com/company/microsoft/misc/ReverseInt.java
@@ -0,0 +1,16 @@
+package com.company.microsoft.misc;
+
+public class ReverseInt {
+    public static void main(String[] args) {
+
+    }
+
+    public static  int reverse(int x) {
+        int rev = 0 ;
+        while(x != 0){
+
+        }
+        return rev;
+    }
+
+}
